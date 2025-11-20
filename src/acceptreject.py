92f9f7fea820ec1af641eb8a56e2bb390100ad7d
Ishita7078 <ishumehta@gmail.com>
2025-11-20 15:37:12 -0700

    accept-reject

---
+++ src/acceptreject.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def rejection_sampling(f, g, sample_g, m, n, rng=None):
+    if rng is None:
+        rng = np.random.default_rng()
+
+    samples = []
+    while len(samples) < n:
+        x = sample_g(1)[0]
+        u = rng.uniform()
+        if u < f(x) / (m * g(x)):
+            samples.append(x)
+    return np.array(samples)
